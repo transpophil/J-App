862e25669d044963ef977b1b1493f13dca9c2b27
[dyad] <transpophil@gmail.com>
2025-11-05 00:31:28 +0100

    [dyad] Fix GitHub Pages 404 by using HashRouter - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { DriverProvider } from "@/contexts/DriverContext";
 import Splash from "./pages/Splash";
 import Login from "./pages/Login";
@@ -18,7 +18,7 @@ const App = () => (
       <DriverProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Splash />} />
             <Route path="/login" element={<Login />} />
@@ -27,7 +27,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </DriverProvider>
     </TooltipProvider>
   </QueryClientProvider>
